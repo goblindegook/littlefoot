76a9cd5077a1f8409bbb87279ac89dbd0bfea0cb
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-02-29 02:01:53 +0000

    Tests with Tape + Testling

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-import test from 'ava'
-// import littlefoot from '../src/'
+import test from 'tape'
+import littlefoot from '../src/'
 
 test('TODO', t => {
-  // TODO: These tests must run in the browser or with jsdom/phantomjs.
+  t.pass('These tests must run in the browser.')
+  t.end()
 })
