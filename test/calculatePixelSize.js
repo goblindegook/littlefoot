eadc1508cb43849e2331c4dc21f27d5af02e528f
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-03-29 01:03:31 +0100

    Test fixtures

--- test/calculatePixelSize.js
+++ test/calculatePixelSize.js
@@ -16,8 +16,8 @@ function setup() {
  * Tear down fixtures.
  */
 function teardown() {
-  Array.prototype.forEach.call(document.body.children, fixture => {
-    document.body.removeChild(fixture)
+  Array.prototype.forEach.call(document.body.children, child => {
+    document.body.removeChild(child)
   })
 }
 
@@ -36,7 +36,7 @@ test('calculatePixelSize (none)', t => {
   const fixture = setup()
 
   fixture.style.display = 'none'
-  t.equals(calculatePixelSize(fixture, 'display'), 10000, 'fixture property is none')
+  t.equal(calculatePixelSize(fixture, 'display'), 10000, 'fixture property is none')
 
   teardown()
   t.end()
@@ -47,11 +47,11 @@ test('calculatePixelSize (px)', t => {
 
   fixture.style.width = '100px'
 
-  t.equals(calculatePixelSize(fixture, 'width'), 100, 'fixture property in px')
+  t.equal(calculatePixelSize(fixture, 'width'), 100, 'fixture property in px')
 
   fixture.style.width = '50px'
 
-  t.equals(calculatePixelSize(fixture, 'width'),
+  t.equal(calculatePixelSize(fixture, 'width'),
     50 / parseFloat(getElementStyle(fixture.parentNode, 'width')),
     'fixture property <= 60px')
 
@@ -64,12 +64,12 @@ test('calculatePixelSize (em)', t => {
 
   fixture.style.width = '10em'
 
-  t.equals(calculatePixelSize(fixture, 'width'), 160,
+  t.equal(calculatePixelSize(fixture, 'width'), 160,
     'fixture property in em with default font size')
 
   fixture.style.fontSize = '200%'
 
-  t.equals(calculatePixelSize(fixture, 'width'), 320,
+  t.equal(calculatePixelSize(fixture, 'width'), 320,
     'fixture property in em with 200% font size')
 
   teardown()
@@ -80,11 +80,11 @@ test('calculatePixelSize (rem)', t => {
   const fixture = setup()
 
   fixture.style.width = '10rem' // 160px assuming 16px default font size
-  t.equals(calculatePixelSize(fixture, 'width'), 160,
+  t.equal(calculatePixelSize(fixture, 'width'), 160,
     'fixture property in rem')
 
   fixture.style.fontSize = '200%'
-  t.equals(calculatePixelSize(fixture, 'width'), 160,
+  t.equal(calculatePixelSize(fixture, 'width'), 160,
     'fixture property in rem with 200% font size')
 
   teardown()
@@ -96,7 +96,7 @@ test('calculatePixelSize (%)', t => {
 
   fixture.style.width = '50%'
 
-  t.equals(calculatePixelSize(fixture, 'width'),
+  t.equal(calculatePixelSize(fixture, 'width'),
     parseFloat(getElementStyle(fixture, 'width')),
     'fixture property in %')
 
--- test/index.js
+++ test/index.js
@@ -1,7 +1,87 @@
 import test from 'tape'
+import escape from 'lodash/escape'
 import littlefoot from '../src/'
+import { dispatchEvent } from '../src/events'
 
-test('TODO', t => {
-  t.pass('These tests must run in the browser.')
-  t.end()
+const fs = require('fs')
+
+/**
+ * Setup fixtures.
+ */
+function setup() {
+  const content = document.createElement('div')
+  content.innerHTML = fs.readFileSync(__dirname + '/fixtures/content.html', 'utf8')
+  document.body.appendChild(content)
+}
+
+/**
+ * Tear down fixtures.
+ */
+function teardown() {
+  Array.prototype.forEach.call(document.body.children, child => {
+    document.body.removeChild(child)
+  })
+}
+
+test('littlefoot setup with default options', t => {
+  t.plan(11)
+
+  setup()
+
+  const body             = document.body
+  const footnotes        = body.querySelectorAll('.footnote').length
+  const reverseFootnotes = body.querySelectorAll('.reversefootnote').length
+
+  const lf = littlefoot()
+
+  const createDelay  = lf.get('popoverCreateDelay')
+  const dismissDelay = lf.get('popoverDismissDelay')
+
+  t.equal(body.querySelectorAll('.littlefoot-footnote__container').length, footnotes,
+    'inserts footnote containers')
+
+  t.equal(body.querySelectorAll('button').length, footnotes,
+    'inserts footnote buttons')
+
+  t.equal(body.querySelectorAll('.footnote-processed').length, reverseFootnotes,
+    'processes footnotes')
+
+  t.equal(body.querySelectorAll('.footnotes').length, 1,
+    'adds a footnote container')
+
+  t.equal(body.querySelectorAll('.footnotes.footnote-print-only').length, 1,
+    'hides the footnote container')
+
+  t.equal(body.querySelectorAll('hr.footnote-print-only').length, 1,
+    'hides the footnote separator')
+
+  t.equal(body.querySelectorAll('li.footnote-print-only').length, reverseFootnotes,
+    'hides all footnotes')
+
+  t.equal(body.querySelectorAll('.littlefoot-footnote__content').length, 0,
+    'has no active footnotes')
+
+  const firstFootnote = body.querySelectorAll('button')[0]
+
+  dispatchEvent(firstFootnote, 'click')
+
+  setTimeout(() => {
+    const content = body.querySelector('.littlefoot-footnote__content')
+
+    t.ok(content, 'has one active popover on activating the footnote')
+
+    t.equal(content.innerHTML, firstFootnote.getAttribute('data-littlefoot-footnote'),
+      'has the popover content matching stored content')
+
+    dispatchEvent(body, 'click')
+
+    setTimeout(() => {
+      t.notOk(body.querySelector('.littlefoot-footnote__content'),
+        'dismisses footnote when clicking outside the popover')
+
+      teardown()
+
+    }, dismissDelay)
+
+  }, createDelay)
 })
