25fefcbeb3fe735214d47fa82460ba0b90d7073a
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-02-16 12:34:39 +0000

    Reorganize tests

--- test/options/allowDuplicates.test.ts
+++ test/options/allowDuplicates.test.ts
@@ -1,19 +1,19 @@
 import littlefoot from '../../src'
 import { setup, queryAllButtons } from '../helper'
 
-test('allowDuplicates: false ignores duplicate footnote references', () => {
-  setup('default.html')
-  littlefoot({ allowDuplicates: false })
-  expect(queryAllButtons()).toHaveLength(3)
-})
-
-test('allowDuplicates: true creates one buttons per footnote reference', () => {
+test('create one button per footnote reference', () => {
   setup('default.html')
   littlefoot({ allowDuplicates: true })
   expect(queryAllButtons()).toHaveLength(4)
 })
 
-test('allowDuplicates: false creates all buttons when footnotes are in different containers', () => {
+test('ignore duplicate footnote references', () => {
+  setup('default.html')
+  littlefoot({ allowDuplicates: false })
+  expect(queryAllButtons()).toHaveLength(3)
+})
+
+test('create all buttons when footnotes are in different containers', () => {
   setup('multiple.html')
   littlefoot({ allowDuplicates: false })
   expect(queryAllButtons()).toHaveLength(4)
