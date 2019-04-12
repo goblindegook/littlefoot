ee4e442afaa6bae0fdbc52618a5369ebd97ad136
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-04-12 18:53:08 +0100

    Split tests

--- test/activate.test.ts
+++ test/activate.test.ts
@@ -15,46 +15,56 @@ beforeEach(() => {
 
 test('activate footnote when clicking the button', async () => {
   littlefoot(TEST_SETTINGS)
-
   const button = getButton('1')
+
   fireEvent.click(button)
 
   expect(button).toHaveClass('is-changing')
   await waitForChange(button)
   expect(button).toHaveClass('is-active')
-
   getPopoverByText(/This is the document's only footnote./)
 })
 
 test('activate footnote by ID when calling .activate()', async () => {
   const instance = littlefoot(TEST_SETTINGS)
-
   const button = getButton('1')
+
   instance.activate('1')
 
   expect(button).toHaveClass('is-changing')
-  await wait(() => expect(button).not.toHaveClass('is-changing'))
-
-  const popover = document.querySelector<HTMLElement>('.littlefoot-footnote')
-  const content = document.querySelector<HTMLElement>(
-    '.littlefoot-footnote__content'
-  )
-
+  await waitForChange(button)
   expect(button).toHaveClass('is-active')
-  expect(button).toHaveAttribute('aria-expanded', 'true')
-  expect(button).toHaveAttribute('aria-controls', popover!.id)
-
-  expect(content).toContainHTML(`This is the document's only footnote.`)
-  expect(content).toHaveProperty('offsetWidth', document.body.clientWidth)
-
-  expect(popover).toHaveAttribute('data-footnote-max-height', '0')
-  expect(popover).toHaveStyle(`max-width: ${document.body.clientWidth}px`)
+  getPopoverByText(/This is the document's only footnote./)
 })
 
 test('activation with unknown ID does not activate any popovers', () => {
   const instance = littlefoot({ activateDelay: 0 })
-
   instance.activate('invalid')
-
   expect(document.querySelector('.littlefoot-footnote')).toBeNull()
+})
+
+test('popup ARIA properties', async () => {
+  littlefoot(TEST_SETTINGS)
+  const button = getButton('1')
+
+  fireEvent.click(button)
+
+  await waitForChange(button)
+  const popover = document.querySelector('.littlefoot-footnote')
+  expect(button).toHaveAttribute('aria-expanded', 'true')
+  expect(button).toHaveAttribute('aria-controls', popover!.id)
+})
+
+test('popup layout dimensions', async () => {
+  littlefoot(TEST_SETTINGS)
+  const button = getButton('1')
+
+  fireEvent.click(button)
+
+  await waitForChange(button)
+  const popover = document.querySelector('.littlefoot-footnote')
+  const content = document.querySelector('.littlefoot-footnote__content')
+  expect(content).toHaveProperty('offsetWidth', document.body.clientWidth)
+  expect(popover).toHaveAttribute('data-footnote-max-height', '0')
+  expect(popover).toHaveStyle(`max-width: ${document.body.clientWidth}px`)
 })
