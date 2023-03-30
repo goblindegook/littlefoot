fe915bb772ffc671136c7e7697cc8e0187eaacbc
Luis Rodrigues <lrodrigues@newstore.com>
2023-03-31 00:25:55 +0200

    chore(deps): switch to @rollup/terser-plugin and remove unnecessary linter plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 import pkg from './package.json'
 
 const production = !process.env.ROLLUP_WATCH
